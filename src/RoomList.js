1487e2b61e8a2de8a7a44989ebe3c7e82bae4bfd
Sean Ryan <seanpryan89@gmail.com>
2019-01-12 14:52:38 -0500

    working on creating user input for new chatrooms

--- src/RoomList.js
+++ src/RoomList.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import App from './App';
+import React, { Component } from "react";
+import App from "./App";
 
 class RoomList extends React.Component {
   constructor(props) {
@@ -7,27 +7,45 @@ class RoomList extends React.Component {
     this.state = {
       rooms: []
     };
-    this.roomsRef = this.props.firebase.database().ref('rooms');
+    this.roomsRef = this.props.firebase.database().ref("rooms");
   }
 
-
-
   componentDidMount() {
-    this.roomsRef.on('child_added', snapshot => {
+    this.roomsRef.on("child_added", snapshot => {
       const room = snapshot.val();
       room.key = snapshot.key;
-      this.setState({ rooms: this.state.rooms.concat( room ) })
+      this.setState({ rooms: this.state.rooms.concat(room) });
+    });
+  }
+
+  createRooms(newRoomName) {
+    this.roomsRef.push({
+      name: newRoomName,
     });
   }
 
   render() {
-    return(
-    <div>
-      {this.state.rooms.map((room, i) => (
-        <p key={i}>Room name: {room.name}</p>
-    ))}
-    </div>);
+    return (
+      <React.Fragment>
+        <div>
+          {this.state.rooms.map((room, i) => (
+            <p key={i}>Room name: {room.name}</p>
+          ))}
+        </div>
+
+        <div className="new-room-form">
+          <h1>Add a new chatroom!</h1>
+          <form>
+            <label for="roomName">Room Name: </label>
+            <input type="text" id="roomName" value="New Room Name"/>
+            <input type="submit" value="Submit"
+              onClick={this.createRooms('test')}
+            />
+          </form>
+        </div>
+      </React.Fragment>
+    );
   }
-};
+}
 
 export default RoomList;
